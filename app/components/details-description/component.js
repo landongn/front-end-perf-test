463811e2e65ce8b6cfb557d8fefbd58f25600af1
Lion Studios <lion.studios94@gmail.com>
2017-05-07 13:10:03 -0700

    start adding in new transitions for details page

--- app/components/details-description/component.js
+++ app/components/details-description/component.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNameBindings: ['transitionIn'],
+  didRender() {
+    var self = this;
+    setTimeout(function () {
+      self.set('transitionIn', true);
+    });
+  },
+  transitionIn: false,
 });
